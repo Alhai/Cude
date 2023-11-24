49c3be1de1f1e4883d1d4b70b4be439c11877eed
Alhai <aly.haidara@seira-formation.fr>
2023-11-24 09:17:45 +0100

    wip

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './Homepage/homepage';
 import Quickstart from './Game/quick-start';
-
+import { HashRouter as Router } from 'react-router-dom';
 const AppRoutes = () => {
     return (
+        <Router>
         <Routes>
             <Route path="/" Component={HomePage} />
             <Route path="/quick-start" Component={Quickstart} />
         </Routes>
+        </Router>
     );
 };
 
